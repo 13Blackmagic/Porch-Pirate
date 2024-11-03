0c95d542426bd155365a38028068bd04eb66eb0b
unknown <anthony.lanier@outlook.com>
2024-11-02 18:17:39 -0600

    added function to app.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Porch-Pirate</h1>
+                <button>New game</button>
+            </header>
+        </div>
+    )
+}
+
+export default App
